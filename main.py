d46ff98021c957507fda8be6fb71ef31c60bf006
Prashant-Jhim-2 <prashantjhim2023@gmail.com>
2025-03-07 12:14:56 -0500

    First

--- main.py
+++ main.py
@@ -13,11 +13,24 @@ from passlib.pwd import genword
 
 
 app = FastAPI()
-
+load_dotenv()
 from fastapi.middleware.cors import CORSMiddleware
 
+firebase_config = {
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),  # Fix multiline private key
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain":os.getenv("UNIVERSE_DOMAIN")
+}
 
-cred = credentials.Certificate("simple.json")
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 usersref = db.collection("users")
